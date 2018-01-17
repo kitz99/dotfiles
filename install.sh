faea2b01e08def1052548ee41dfedc2e31c1b560
Bogdan Timofte <bogdan.timofte@sparktch.ro>
2018-01-17 13:33:32 +0200

    Add custom vim

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt-get install zsh
 sudo apt-get install git-core
+sudo apt-get install vim
+
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 mv $HOME/.zshrc $HOME/.zshrc.BKP
 cp $HOME/dotfiles/zshrc_sample $HOME/.zshrc
 
+cp $HOME/dotfiles/customvim.zip $HOME/customvim.zip
+unzip $HOME/customvim.zip
+rm -rf $HOME/customvim.zip
+
 touch $HOME/dotfiles/vars/.creds
 
 chsh -s `which zsh`
+
+echo "Please rebot."

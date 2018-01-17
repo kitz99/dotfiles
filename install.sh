2b8ec76e9f30fa19002698e0699c13d1de233346
Bogdan Timofte <bogdan.timofte@sparktch.ro>
2018-01-17 15:56:35 +0200

    add terminator config file

--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@ sudo apt-get update
 sudo apt-get install zsh
 sudo apt-get install git-core
 sudo apt-get install vim
-
+sudo apt-get install terminator
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 mv $HOME/.zshrc $HOME/.zshrc.BKP
-cp $HOME/dotfiles/zshrc_sample $HOME/.zshrc
+ln $HOME/dotfiles/zshrc_sample $HOME/.zshrc
 
 unzip customvim.zip -d $HOME
+
+ln $HOME/dotfiles/terminator_config $HOME/.config/terminator/config
 
 touch $HOME/dotfiles/vars/.creds
 
